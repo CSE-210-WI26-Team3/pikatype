99f7576e93b13129bf0c1b1d6013db590a811a9b
Luke Dinh <lukexdinh@gmail.com>
2026-02-04 15:20:38 -0800

    chore: switched to base-ui instead for unstyled components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,14 @@
-import { Button as MuiButton, ButtonProps as MuiButtonProps } from "@mui/material";
+import {
+  Button as BaseButton,
+  ButtonProps as BaseButtonProps,
+} from "@base-ui/react";
 
-interface ButtonProps extends MuiButtonProps {
+interface ButtonProps extends BaseButtonProps {
   label: string;
 }
 
 function Button({ label, ...props }: ButtonProps) {
-  return <MuiButton {...props}>{label}</MuiButton>;
+  return <BaseButton {...props}>{label}</BaseButton>;
 }
 
 export default Button;
