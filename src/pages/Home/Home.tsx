2090edd9535ef40d8d1236f9c4d384db810e0756
Thanh Trinh <thanhnghiep1007@gmail.com>
2026-02-04 16:50:33 -0800

    Feature: Static homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,25 @@
 import { useNavigate } from "react-router";
-import Button from "../../components/Button";
+import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h2>Pikatype</h2>
-      <Button label="Levels" onClick={() => navigate("/levels")} />
-      <Button label="Battle" onClick={() => navigate("/battle")} />
+    <div className="home-container">
+      <div className="title-box">
+        <h1 className="title-text">PikaType</h1>
+      </div>
+
+      <div className="menu-buttons">
+        <button className="menu-button" onClick={() => navigate("/levels")}>
+          New Game
+        </button>
+        <button className="menu-button" onClick={() => navigate("/battle")}>
+          Continue
+        </button>
+        <button className="menu-button">Options</button>
+        <button className="menu-button">Save & Quit</button>
+      </div>
     </div>
   );
 }
