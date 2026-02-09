b0e0ac0beea8e40ff4d22b77ba6dcca28b1ebdb7
Jayden <112384009+jayden789@users.noreply.github.com>
2026-02-09 13:15:51 -0800

    Thanh/static home page (#13)
    * Feature: Static homepage
    
    * Update: change file path
    
    * Fix: Refractor code to use Button component
    
    * Fix: Remove Main Menu.png
    
    * Update: Restyle Home.module.css and rename background img

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,30 @@
 import { useNavigate } from "react-router";
 import Button from "../../components/Button";
+import styles from "./Home.module.css";
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h2>Pikatype</h2>
-      <Button label="Levels" onClick={() => navigate("/levels")} />
-      <Button label="Battle" onClick={() => navigate("/battle")} />
+    <div className={styles.homeContainer}>
+      <div className={styles.titleBox}>
+        <h1 className={styles.titleText}>PikaType</h1>
+      </div>
+
+      <div className={styles.menuButtons}>
+        <Button
+          label="New Game"
+          className={styles.menuButton}
+          onClick={() => navigate("/levels")}
+        />
+        <Button
+          label="Continue"
+          className={styles.menuButton}
+          onClick={() => navigate("/battle")}
+        />
+        <Button label="Options" className={styles.menuButton} />
+        <Button label="Save & Quit" className={styles.menuButton} />
+      </div>
     </div>
   );
 }
