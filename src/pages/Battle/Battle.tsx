0f8aedbd05d091f999cddea816047bed0eecf795
Luke Dinh <lukexdinh@gmail.com>
2026-02-09 14:22:09 -0800

    feat: started styling battle screen, added pokemon battle scene

--- src/pages/Battle/Battle.tsx
+++ src/pages/Battle/Battle.tsx
@@ -1,14 +1,44 @@
 import BattleTimer from "./Timer";
 import TimerProvider from "./Timer/TimerProvider";
 import HealthBar from "./HealthBar";
-
+import styles from "./Battle.module.css";
 function Battle() {
   return (
-    <div>
-      <h1>Battle</h1>
+    <div className={styles.battleContainer}>
+      <h1 className={styles.battleTitle}>Battle</h1>
       <TimerProvider time={60}>
         <BattleTimer />
-        <HealthBar />
+        <div className={styles.battleScene}>
+          <div className={styles.playerPokemonContainer}>
+            <HealthBar />
+            <div className={styles.imagesContainer}>
+              <img
+                className={styles.playerPokemon}
+                src="/img/pokemon/piplup.png"
+              />
+              <img
+                className={styles.grassPatch}
+                src="/img/grass_patch.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className={styles.wildPokemonContainer}>
+            <HealthBar />
+            <div className={styles.imagesContainer}>
+              <img
+                className={styles.wildPokemon}
+                src="/img/pokemon/bidoof.png"
+                alt=""
+              />
+              <img
+                className={styles.grassPatch}
+                src="/img/grass_patch.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
       </TimerProvider>
     </div>
   );
