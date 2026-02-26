67c768b518b28b7090d66430ff5dede118fa4193
Aniket Gupta <newracket@users.noreply.github.com>
2026-02-25 16:19:06 -0800

    Added starter pokemon selection (#31)
    * Added saving to localstorage
    
    * Fixed JSON.parse error
    
    * Addressed feedback
    
    * Added pokemon images
    
    * Added starter pokemon selection
    
    * Added unit tests for starter select
    
    * Changed pokemon sprites to gen 5

--- src/pages/Battle/Battle.tsx
+++ src/pages/Battle/Battle.tsx
@@ -1,12 +1,18 @@
+import { useMemo } from "react";
 import TypingTrackerProvider from "../../components/TypingTracker/TypingTrackerProvider";
 import TypingTrackerView from "../../components/TypingTracker/TypingTrackerView";
 import BattleTimer from "./Timer";
 import TimerProvider from "./Timer/TimerProvider";
 import styles from "./Battle.module.css";
 import { PlayerHealthBar } from "./HealthBar/HealthBar";
+import { Save } from "../../components/Storage/Save";
 import { SingleWordGenerator } from "../../wordGeneration/index";
 
+const NUM_LEVELS = 3;
+
 function Battle() {
+  const save = useMemo(() => new Save(NUM_LEVELS), []);
+  const starterPokemon = save.getStarter() || "bulbasaur";
   return (
     <div className={styles.battleContainer}>
       <h1 className={styles.battleTitle}>Battle</h1>
@@ -21,7 +27,7 @@ function Battle() {
               <div className={styles.imagesContainer}>
                 <img
                   className={styles.playerPokemon}
-                  src={process.env.PUBLIC_URL + "/img/pokemon/piplup.png"}
+                  src={process.env.PUBLIC_URL + `/img/pokemon/${starterPokemon}.png`}
                   alt="player pokemon sprite"
                 />
                 <img
