912149a2ba444732f619115bda1705313879c0d0
sansv8 <amith.p.pang@gmail.com>
2026-03-08 13:48:15 -0700

    Add test cases

--- src/components/Tutorial/TutorialView.tsx
+++ src/components/Tutorial/TutorialView.tsx
@@ -8,27 +8,35 @@ import "./TutorialView.css"
 import '@coreui/coreui/dist/css/coreui.min.css';
 import { Dialog } from '@base-ui/react';
 
+type TutorialImage = {
+  ImagePath : string;
+  ImageDescription: string;
+  ImageKey: string, 
+};
 
-function TutorialView(){
+type TutorialConfig = {
+  TutorialImages: TutorialImage[];
+};
+
+function TutorialView(Config: TutorialConfig){
+  const TutorialImage = Config.TutorialImages.map(TutorialImage =>
+    <CCarouselItem key={TutorialImage.ImageKey}>
+      <CImage className="d-block CImageContainer" src={process.env.PUBLIC_URL + TutorialImage.ImagePath} alt={TutorialImage.ImageKey}></CImage>
+      <CCarouselCaption className="d-none d-md-block">
+        <h1>{TutorialImage.ImageDescription}</h1>
+      </CCarouselCaption>
+     </CCarouselItem>
+  )
+
   return (
+    <div>
        <Dialog.Root defaultOpen={true}>
       <Dialog.Portal>
         <Dialog.Backdrop className="Backdrop" />
         <Dialog.Popup className="Popup">
           <Dialog.Title className="Title">Tutorial</Dialog.Title>
           <CCarousel className="CarouselContainer" interval={false} dark wrap={false} controls indicators>
-              <CCarouselItem>
-                <CImage className="d-block CImageContainer" src={process.env.PUBLIC_URL + "/img/tutorial/image1.jpg"}></CImage>
-                <CCarouselCaption className="d-none d-md-block">
-                  <h1>Some representative placeholder content for the first slide.</h1>
-                </CCarouselCaption>
-              </CCarouselItem>
-              <CCarouselItem>
-                <CImage className="d-block CImageContainer" src={process.env.PUBLIC_URL + "/img/tutorial/image2.png"}></CImage>
-                   <CCarouselCaption className="d-none d-md-block">
-                  <h1>Some representative placeholder content for the first slide.</h1>
-                </CCarouselCaption>
-              </CCarouselItem>
+              {TutorialImage}
             </CCarousel>
           <div className="Actions">
             <Dialog.Close className="Button">Close</Dialog.Close>
@@ -36,7 +44,9 @@ function TutorialView(){
         </Dialog.Popup>
       </Dialog.Portal>
     </Dialog.Root>
+    </div>
   );
 }
 
 export default TutorialView
+export type {TutorialImage, TutorialConfig}
